9db7b3e86a392d6011d8df88f1432b2624638aa6
Michael Preston <56261701+99mpreston@users.noreply.github.com>
2019-10-07 06:41:49 -0700

    Add files via upload
    Step 1.

---
+++ AddingMachine.java
@@ -0,0 +1,58 @@
+/**
+ * A class for summations.
+ */
+public class AddingMachine {
+    private int total = 0;
+    
+    /**
+     * Class constructor, no arguments.
+     * 
+     * @return An AddingMachine object
+     */
+    public AddingMachine() {
+        total = 0;
+    }
+
+    /**
+     * Returns the total of an AddingMachine.
+     * 
+     * @return AddingMachine.total
+     */
+    public int getTotal() {
+        return 0;
+    }
+
+    /**
+     * Adds a value to an AddingMachine.
+     * 
+     * @param value the value to be added
+     */
+    public void add(int value) {
+        return;
+    }
+
+    /**
+     * Subtracts a value from an AddingMachine.
+     * 
+     * @param value the value to be subtracted
+     */
+    public void subtract(int value) {
+        return;
+    }
+
+    /**
+     * Displays information regarding an AddingMachine.
+     * 
+     * @return A string with the object's values
+     */
+    public String toString() {
+        return "";
+    }
+
+    /**
+     * Clears the total from an AddingMachine.
+     */
+    public void clear(){
+        return;
+    }
+}
